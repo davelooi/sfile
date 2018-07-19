7038a6e9654fdfbf0a1b8fe5c82a992dcdf3cbfc
David Looi <david@cultureamp.com>
2018-07-19 11:42:27 +1000

    Allow static files to be accessed with basic auth

--- main.rb
+++ main.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+set :public_folder, Proc.new { File.join(root, 'public') }
+
+use Rack::Auth::Basic, 'Authentication required' do |username, password|
+  username == ENV.fetch('USERNAME') && password == ENV.fetch('PASSWORD')
+end
+
 get '/' do
   'Hello World!'
 end
