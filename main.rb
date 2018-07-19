24a50bdee09b0a7cb4c46ab05d6209b96b5527e1
David Looi <david@cultureamp.com>
2018-07-19 12:05:06 +1000

    Allow file to be uploaded to the public dir

--- main.rb
+++ main.rb
@@ -3,9 +3,17 @@ require 'sinatra'
 set :public_folder, Proc.new { File.join(root, 'public') }
 
 use Rack::Auth::Basic, 'Authentication required' do |username, password|
-  username == ENV.fetch('USERNAME') && password == ENV.fetch('PASSWORD')
+  username == ENV.fetch('SFILE_USERNAME') && password == ENV.fetch('SFILE_PASSWORD')
 end
 
 get '/' do
   'Hello World!'
+end
+
+post '/upload' do
+  file = params[:file]
+  filename = file[:filename]
+  tempfile = file[:tempfile]
+  File.open(File.join('public', filename), 'w') { |f| f.write tempfile.read }
+  redirect '/'
 end
